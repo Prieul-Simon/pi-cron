a35479b56a3d980f9b6a4f90f151eaec51af11a9
Simon Prieul <simon.prieul@gmail.com>
2026-03-10 18:18:17 +0100

    Modify docker exec cmd

--- sh/renew-cert.bash
+++ sh/renew-cert.bash
@@ -6,7 +6,7 @@ run () {
     local CONTAINER_NAME="$(/usr/bin/docker ps --format '{{.Names}}' | grep -E 'certbot-prieulfr' | head -1)"
 
     # Execute sh file from bound volume
-    /usr/bin/docker exec -i $CONTAINER_NAME /bin/sh -c '/certbot-work/call_certbot.sh'
+    /usr/bin/docker exec -i $CONTAINER_NAME /bin/sh -c 'cd /certbot-work && ./call_certbot.sh'
 
     echo "END $FILE_NAME"
 }
