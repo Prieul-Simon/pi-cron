5edf4c2b7436bd3fb89e514a508291d5905297a5
Simon Prieul <simon.prieul@gmail.com>
2026-03-10 17:36:06 +0100

    Fix: do not allocate tty

--- sh/test-alpine.bash
+++ sh/test-alpine.bash
@@ -6,7 +6,7 @@ run () {
     local CONTAINER_NAME="$(/usr/bin/docker ps --format '{{.Names}}' | grep -E 'sys-alpine' | head -1)"
 
     # Do call the Alpine container
-    /usr/bin/docker exec -it $CONTAINER_NAME /bin/sh -c '. /etc/os-release && echo "Container OS name is: $PRETTY_NAME"'
+    /usr/bin/docker exec -i $CONTAINER_NAME /bin/sh -c '. /etc/os-release && echo "Container OS name is: $PRETTY_NAME"'
 
     echo "END $FILE_NAME"
 }
