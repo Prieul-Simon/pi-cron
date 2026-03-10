9b6aaa2dc11d7a6ce584ee98d96e05e363445ef8
Simon Prieul <simon.prieul@gmail.com>
2026-03-10 22:49:38 +0100

    Update cert renewal

--- sh/renew-cert.bash
+++ sh/renew-cert.bash
@@ -3,10 +3,29 @@ run () {
     local FILE_NAME='renew-cert.bash'
     echo "BEGIN $FILE_NAME"
 
+    # Test
+    if [[ -z "$CURRENT_USER_LOC" ]]; then
+        echo "CURRENT_USER_LOC is empty, cannot renew cert"
+        exit 1
+    fi
+
     local CONTAINER_NAME="$(/usr/bin/docker ps --format '{{.Names}}' | grep -E 'certbot-prieulfr' | head -1)"
 
     # Execute sh file from bound volume
     /usr/bin/docker exec -i $CONTAINER_NAME /bin/sh -c 'cd /certbot-work && ./call_certbot.sh'
+
+    # tmp debug code
+    local LAST_RETURN_CODE=$?
+    echo "last return code is: $LAST_RETURN_CODE"
+
+    # copy files to the expected location and update symbolic links
+    local DATE_SUFFIX="$(date +%Y)-$(date +%m)"
+    local VOLUME_OUTPUT_DIR="$CURRENT_USER_LOC/storage/microsd/data/certbot-prieulfr/etc-letsencrypt/live/prieul.fr"
+    local NGINX_DIR="$CURRENT_USER_LOC/cert/prieul.fr"
+    cp "$VOLUME_OUTPUT_DIR/fullchain.pem" "$NGINX_DIR/fullchain-$DATE_SUFFIX.pem"
+    cp "$VOLUME_OUTPUT_DIR/privkey.pem" "$NGINX_DIR/privkey-$DATE_SUFFIX.pem"
+    ln -sf "fullchain-$DATE_SUFFIX.pem" "$NGINX_DIR/fullchain-fixme.pem"
+    ln -sf "privkey-$DATE_SUFFIX.pem" "$NGINX_DIR/privkey-fixme.pem"
 
     echo "END $FILE_NAME"
 }
--- sh/schedule-renew-cert.sh
+++ sh/schedule-renew-cert.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-# Certbot only renews certificates within 30 days of expiration, so running it daily is safe
 
 # Every 5 minute, and pipe output to cron service output
 CRON_CONFIG='*/5 * * * *'
-# At 04:58 on day(s) 15th in March, June, September, December
-# CRON_CONFIG='58 4 15 3,6,9,12 *'
+# Certbot only renews certificates within 30 days of expiration, so running it daily is safe
+# At 04:58 everyday
+# CRON_CONFIG='58 4 * * *'
+
+# Need current user
+CURRENT_USER_LOC=$HOME
+CRONTAB_ENV_SETTER="CURRENT_USER_LOC=$CURRENT_USER_LOC"
 
 SCRIPT_ABS_PATH=$(realpath ./renew-cert.bash)
 LOGGER_TAG='cron_renew_cert'
 
 # Whole line that will be appended to crontab
-CRON_JOB="$CRON_CONFIG $SCRIPT_ABS_PATH 2>&1 | logger --tag $LOGGER_TAG"
+CRON_JOB="$CRON_CONFIG $CRONTAB_ENV_SETTER; $SCRIPT_ABS_PATH 2>&1 | logger --tag $LOGGER_TAG"
 
 # Do the appending
 crontab -l | { cat; echo "$CRON_JOB"; } | crontab -
