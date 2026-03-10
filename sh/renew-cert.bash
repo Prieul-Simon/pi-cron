a3799c9932e9cdd44f3504cd8296d77b29a0b41e
Simon Prieul <simon.prieul@gmail.com>
2026-03-10 18:12:38 +0100

    Add cron job for cert renewal

---
+++ sh/renew-cert.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+run () {
+    local FILE_NAME='renew-cert.bash'
+    echo "BEGIN $FILE_NAME"
+
+    local CONTAINER_NAME="$(/usr/bin/docker ps --format '{{.Names}}' | grep -E 'certbot-prieulfr' | head -1)"
+
+    # Execute sh file from bound volume
+    /usr/bin/docker exec -i $CONTAINER_NAME /bin/sh -c '/certbot-work/call_certbot.sh'
+
+    echo "END $FILE_NAME"
+}
+run
---
+++ sh/schedule-renew-cert.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Certbot only renews certificates within 30 days of expiration, so running it daily is safe
+
+# Every 2 minute, and pipe output to cron service output
+CRON_CONFIG='*/2 * * * *'
+# At 04:58 on day(s) 15th in March, June, September, December
+# CRON_CONFIG='58 4 15 3,6,9,12 *'
+
+SCRIPT_ABS_PATH=$(realpath ./renew-cert.bash)
+LOGGER_TAG='cron_renew_cert'
+
+# Whole line that will be appended to crontab
+CRON_JOB="$CRON_CONFIG $SCRIPT_ABS_PATH 2>&1 | logger --tag $LOGGER_TAG"
+
+# Do the appending
+crontab -l | { cat; echo "$CRON_JOB"; } | crontab -
